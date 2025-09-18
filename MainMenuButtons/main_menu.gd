6ae70d5f531735c4c8f4ae892df53a465ee57568
RoseTeaDL <ROSEPETALXD0@GMAIL.COM>
2025-09-17 21:10:44 -0400

    Some progress made on making buttons

---
+++ MainMenuButtons/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	print("Start pressed")
+
+func _on_credits_pressed() -> void:
+	print("Credits pressed")
+
+func _on_options_pressed() -> void:
+	print("Options pressed")
