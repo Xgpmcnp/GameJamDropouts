9b2c7a645520e6e80553fe0d3ab2fb579239da67
RoseTeaDL <ROSEPETALXD0@GMAIL.COM>
2025-09-21 11:09:56 -0400

    Help

--- CustomerServiceScreen/Script/player.gd
+++ CustomerServiceScreen/Script/player.gd
@@ -1,11 +1,21 @@
 extends Node2D
+<<<<<<< Updated upstream
 @onready var player_sprite: AnimatedSprite2D = $%AvatarFrame
+=======
+@onready var saigon_happy: Sprite2D = $SaigonHappy
+@onready var saigon_annoyed: Sprite2D = $SaigonAnnoyed
+@onready var saigon_stressed: Sprite2D = $SaigonStressed
+@onready var saigon_depressed: Sprite2D = $SaigonDepressed
+>>>>>>> Stashed changes
 var health: float = 100
 signal health_changed(new_value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	saigon_happy.visible = true
+	saigon_annoyed.visible = false
+	saigon_stressed.visible = false
+	saigon_depressed.visible = false
 
 func _process(delta: float) -> void:
 	pass
@@ -19,6 +29,7 @@ func _update_sprite() -> void:
 	match health:
 		_ when health >= 75:
 			print("Healthy")
+<<<<<<< Updated upstream
 			player_sprite.play("happy")
 		_ when health >= 50:
 			print("Annoyed")
@@ -26,6 +37,17 @@ func _update_sprite() -> void:
 		_ when health >= 25:
 			print("Stress")
 			player_sprite.play("stress")
+=======
+		_ when health >= 50:
+			print("Annoyed")
+			saigon_happy.visible = false
+			saigon_annoyed.visible = true
+		_ when health >= 25:
+			print("Stress")
+			saigon_annoyed.visible = false
+			saigon_stressed.visible = true
+>>>>>>> Stashed changes
 		_ when health < 25:
 			print("Depress")
-			player_sprite.play("depress")
+			saigon_stressed.visible = false
+			saigon_depressed.visible = true
